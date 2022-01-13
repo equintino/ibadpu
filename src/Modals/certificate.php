62f88ac1a48e700bb0685c29e02f98d2380f6493
edmilson <edmquintino@gmail.com>
2022-01-13 15:47:18 -0300

    fix: repaired certificate margin

--- src/Modals/certificate.php
+++ src/Modals/certificate.php
@@ -3,9 +3,65 @@
         background: none;
     }
 
+    #certificate {
+        width: 100%;
+    }
+
+#certificate p {
+    font-size: 2em;
+    color: #6f6e6e;
+    /* margin: 6px 0; */
+}
+
+#certificate .data {
+    color: black;
+    font-family: 'Carattere', cursive;
+    font-size: 3.5em;
+    font-weight: bolder;
+}
+
+#certificate #to p {
+    color: #ac0600;
+}
+
+#certificate #to div {
+    border: 1px solid #b20605;
+    height: 676px;
+    margin: 5px;
+}
+
+#certificate .paragraph {
+    line-height: 1.2em;
+    font-size: 1.5em;
+    padding: 13px 10px;
+}
+
+    #certificate table, #certificate #to {
+        /* margin-bottom: 49px; */
+        border: 2px solid #b20605;
+        margin: 0 auto 49px;
+        cursor: pointer;
+        height: 689px;
+        width: 100%;
+    }
+
+    #certificate #to {
+        text-align: center;
+        font-size: 2.4em;
+        font-weight: bolder;
+        font-family: 'Carattere', cursive;
+    }
+
     #certificate table, #certificate #to {
         background: url("<?= theme('assets/img/logo_marcadagua.png') ?>") no-repeat center;
         background-size: 300px;
+    }
+
+    #certificate .base {
+        margin: 10px 0;
+        color: #b20605;
+        font-size: 1.5em;
+        width: 800px;
     }
 
     @media print {
@@ -27,26 +83,26 @@
         if( preg_match($patern, mb_strtolower($membership->church_baptism, "UTF-8")) ): ?>
             <?php if($side != "false"): ?>
             <!-- Forehead -->
-            <table data-id="<?= $membership->id ?>" widht="100%" border="0" cellspacing="0" title="Clique para ver o verso" data-side=true>
+            <table data-id="<?= $membership->id ?>" border="0" cellspacing="0" title="Clique para ver o verso" data-side=true>
                 <tr>
-                    <td colspan="4" align="center" style="padding-top: 10px;"><img id="title-cert" src="<?= theme('assets/img/title.png') ?>" alt="" height="100px" /></td>
+                    <td colspan="4" align="center" ><img id="title-cert" src="<?= theme('assets/img/title.png') ?>" alt="" height="150px" /></td>
                 </tr>
                 <tr>
-                    <td colspan="4" align="center" ><p style="margin-top: -10px">Certificamos Que</p></td>
+                    <td colspan="4" align="center" ><p class="pb-1" style="margin-top: -40px">Certificamos Que</p></td>
                 </tr>
                 <tr>
                     <td colspan="4" align="center">
-                        <p class="data"><?= abbreviationName(mb_strToLower($membership->name, "UTF-8")) ?></p>
+                        <span class="data"><?= abbreviationName(mb_strToLower($membership->name, "UTF-8")) ?></span>
                     </td>
                 </tr>
                 <tr>
                     <td colspan="4" align="center">
-                        <p>Filho(a) De</p>
+                        <p class="pb-1">Filho(a) De</p>
                     </td>
                 </tr>
                 <tr>
                     <td colspan="4" align="center">
-                        <p><span class="data" ><?= abbreviationName(mb_strToLower($membership->father, "UTF-8"), "wallet") ?></span><span style="font-size: 1.3em; padding: 0 8px">&</span><span class="data"><?= abbreviationName(mb_strToLower($membership->mother, "UTF-8"), "wallet") ?></p>
+                        <span><span class="data" ><?= abbreviationName(mb_strToLower($membership->father, "UTF-8"), "wallet") ?></span><span style="font-size: 2.4em; padding: 0 30px">&</span><span class="data"><?= abbreviationName(mb_strToLower($membership->mother, "UTF-8"), "wallet") ?></span>
                     </td>
                 </tr>
                 <tr>
@@ -55,31 +111,31 @@
                     </td>
                 </tr>
                 <tr>
-                    <td width="43%" style="padding-left: 50px"><p>No Dia:</p></td>
+                    <td width="43%" style="padding: 0 0 8px 50px"><span style="font-size: 1.3em">No Dia:</span></td>
                     <td></td>
                     <td></td>
-                    <td align="center"><p>Foi Batizado(a) na:</p></td>
+                    <td align="center"><span style="font-size: 1.3em">Foi Batizado(a) na:</span></td>
                 </tr>
                 <tr>
-                    <td valign="top" style="padding-left: 50px"><span style="border: 3px solid #b20605; text-align: center; background: #d7bf9b; border-radius: 5px; padding: 4px; font-weight: bolder"> <?= $date[2] ?> / <?= $date[1] ?> / <?= $date[0] ?> </span></td>
+                    <td valign="top" style="padding-left: 50px"><span style="border: 3px solid #b20605; text-align: center; background: #d7bf9b; border-radius: 5px; padding: 4px; font-weight: bolder;font-size: 1.4em"> <?= $date[2] ?> / <?= $date[1] ?> / <?= $date[0] ?> </span></td>
                     <td></td>
                     <td></td>
-                    <td style="padding-bottom: 20px" align="center"><img src="<?= theme("assets/img/nomeLogo.png") ?>" alt="" height="40px"/></td>
+                    <td align="center" valign="top"><img src="<?= theme("assets/img/nomeLogo.png") ?>" alt="" height="50px"/></td>
                 </tr>
                 <tr>
-                    <td align="center" style="padding-bottom: 10px" ><p>__________________________<br>1° Secretário(a)</p></td>
+                    <td align="center" style="padding-bottom: 10px" ><p style="font-size: 1.2em">__________________________<br>1° Secretário(a)</p></td>
                     <td width="30px"></td>
                     <td width="30px"></td>
-                    <td align="center" style="padding-bottom: 10px" ><p>__________________________<br>Pr. Presidente</p></td>
+                    <td align="center" style="padding-bottom: 10px" ><p style="font-size: 1.2em">__________________________<br>Pr. Presidente</p></td>
                 </tr>
             <table>
             <?php else: ?>
             <!-- towards -->
-            <div id="to" title="Clique para ver a frente" data-id="<?= $membership->id ?>" data-side=false>
+            <div id="to" title="Clique para ver a frente" data-id="<?= $membership->id ?>" data-side=false style="margin-bottom: 100px">
                 <div>
-                    <p><img src="<?= theme("assets/img/title_pacto.png") ?>" alt="" height="42px"/></p>
+                    <p style="margin-top: 20px"><img src="<?= theme("assets/img/title_pacto.png") ?>" alt="" height="62px"/></p>
                     <p class="paragraph">Tendo sido levado, como cri, pelo Espírito de Deus a aceitar e confessar o Senhor Jesus Cristo, como meu único e suficiente Salvador, e tendo sido batizado sob a minha fé em nome do Pai, do Filho e do Espírito Santo, agora, na presença de Deus, dos anjos e desta congregação, muito solene e alegremente, comprometo-me buscar a minha caminhada cristã as seguintes virtudes:</p>
-                    <p><img src="<?= theme("assets/img/virtudes.png") ?>" alt="" height="40px"/></p>
+                    <p><img src="<?= theme("assets/img/virtudes.png") ?>" alt="" height="50px"/></p>
                 </div>
             </div>
             <?php endif; ?>
