5d7541c8e9a651a437b8f5664a84c6b7d61ba538
edmilson <emquintino@gmail.com>
2022-05-04 12:40:03 -0300

    fix: input size from page occupation - responsive

--- src/Modals/occupation.php
+++ src/Modals/occupation.php
@@ -35,15 +35,15 @@
                     <label class="label" for="nome">Nome: </label><br>
                     <input id="nome" name="name" class="form-input" type="text" required="required" value="<?= ($name ?? null) ?>"/></div>
                 <div class="col-md">
-                    <label for="active" class="label mb-3">ATIVO: &nbsp&nbsp</label><br>
-                    <label class="label">SIM </label><input class="form-radio" type="radio" name="active" value=1 <?= (isset($active) && $active == 1 ? "checked" : null) ?> />
-                    <label class="label"> NÃO </label><input type="radio" name="active" value=0 <?= (isset($active) && $active == "0" ? "checked" : null) ?> />
+                    <label for="active" class="label mb-3">Ativo: &nbsp&nbsp</label><br>
+                    <label class="label">sim </label><input class="form-radio" type="radio" name="active" value=1 <?= (isset($active) && $active == 1 ? "checked" : null) ?> />
+                    <label class="label"> não </label><input type="radio" name="active" value=0 <?= (isset($active) && $active == "0" ? "checked" : null) ?> />
                 </div><!-- col -->
             </div>
         </fieldset>
-        <button type="submit" class="button save" style="float: right;"><?= (isset($name) ? "Gravar Alteração" : "Salvar") ?></button>
+        <button type="submit" class="button save mr-1" style="float: right;"><?= (isset($name) ? "Gravar Alteração" : "Salvar") ?></button>
         <?php if(!isset($name)): ?>
-        <button type="reset" class="button cancel" style="float: right;">Limpar</button>
+        <button type="reset" class="button cancel mr-1" style="float: right;">Limpar</button>
         <?php endif ?>
     </form>
 </div><!-- edit -->
